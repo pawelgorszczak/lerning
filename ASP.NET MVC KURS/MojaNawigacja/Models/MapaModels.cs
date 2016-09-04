f19485856e07379d8774388325a9bbd96f8b57dd
pawelgorszczak <pawelgorszczakpl@gmail.com>
2016-09-04 19:53:22 +0200

    Data validation added

---
+++ ASP.NET MVC KURS/MojaNawigacja/Models/MapaModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MojaNawigacja.Models
+{
+    [MetadataType(typeof(AdresyMetaData))]
+    public partial class Adresy
+    { 
+    }
+
+    public class AdresyMetaData
+    {
+        public object IdTrasy { get; set; }
+
+        [Required(ErrorMessage="Pole Miejsce wyjazdu jest wymagane")]
+        [StringLength(50,ErrorMessage="Maksymalna długość adresu wynosi 50 znaków")]
+        [DisplayName("Miejsce wyjazdu")]
+        public object MiejsceWyjazdu { get; set; }
+
+        [Required(ErrorMessage="Pole Miejsce docelowe jest wymagane")]
+        [StringLength(50, ErrorMessage = "Maksymalna długość adresu wynosi 50 znaków")]
+        [DisplayName("Miejsce docelowe")]
+        public object MiejsceDocelowe { get; set; }
+
+        [DisplayName("Data dodania")]
+        public object DataDodania { get; set; }
+    }
+
+}
