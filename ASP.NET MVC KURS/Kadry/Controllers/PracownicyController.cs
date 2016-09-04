e687cb60eea35ce4c7a190cabe2771634bc5cd20
pawelgorszczak <pawelgorszczakpl@gmail.com>
2016-09-04 19:55:40 +0200

    Employee aplicaton, adding new employee, data validation

---
+++ ASP.NET MVC KURS/Kadry/Controllers/PracownicyController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kadry.Models;
+
+namespace Kadry.Controllers
+{
+    public class PracownicyController : Controller
+    {
+        //
+        // GET: /Pracownicy/
+        static List<Pracownik> listaPracownikow = new List<Pracownik>();
+
+
+        public ActionResult UtworzPracownika()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult UtworzPracownika(Pracownik pracownik)
+        {
+            if (ModelState.IsValid)
+            {
+                listaPracownikow.Add(pracownik);
+                return View("ListaPracownikow", listaPracownikow);
+            }
+            return View(pracownik);
+        }
+
+        public ActionResult ListaPracownikow()
+        {
+            return View(listaPracownikow);
+        }
+    }
+}
---
+++ ASP.NET MVC KURS/Kadry/Models/PracownicyModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Kadry.Models
+{
+    public class Pracownik
+    {
+        [Required(ErrorMessage =" Pole Imię jest wymagane")]
+        [DisplayName("Imię")]
+        [StringLength(50, ErrorMessage="Imię nie może być dłuższe niż 50 znaków")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = " Pole Nazwisko jest wymagane")]        
+        [StringLength(50, ErrorMessage = "Nazwisko nie może być dłuższe niż 50 znaków")]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = " Pole Wiek jest wymagane")]
+        [Range(0,100, ErrorMessage = "Wiek powinien mieć wartość z przedziału od 0 do 100")]
+        public int Wiek { get; set; }
+
+        [Required(ErrorMessage = " Pole Stanowisko jest wymagane")]
+        [StringLength(50, ErrorMessage = "Łańcuch znakowy w polu Stanowisko nie może być dłuższy niż 50 znaków")]
+        public string Stanowisko { get; set; }
+
+        [Required(ErrorMessage ="Pole Data Zatrudnienia jest wymagane")]
+        [DataType(DataType.Date)]
+        [DisplayName("Data Zatrudnienia (DD/MM/RRRR)")]
+        [CustomValidationAttribute(typeof(Pracownik), "SprawdzPoprawnoscDatyZatrudnienia")]
+        public System.DateTime DataZatrudnienia { get; set; }
+
+        public static ValidationResult SprawdzPoprawnoscDatyZatrudnienia(System.DateTime data)
+        {
+            if (data.Date > System.DateTime.Now.Date)
+                return new ValidationResult("Data zatrudnienia nie może być póżniejsza od daty dzisiejszej");  
+            return ValidationResult.Success;
+        }
+    }
+}
