b03434d00a8c75a433786ab1421d6c1a151c092c
pawelgorszczak <pawelgorszczakpl@gmail.com>
2016-09-07 19:17:14 +0200

    Ajax basics

---
+++ ASP.NET MVC KURS/MsAjax/Controllers/ListaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MsAjax.Controllers
+{
+    public class ListaController : Controller
+    {
+        //
+        // GET: /Lista/
+        static List<string> listaElementow = new List<string>();
+        
+        public ActionResult Index(FormCollection formCollection)
+        {
+            if (formCollection.Count > 0)
+                listaElementow.Add(formCollection[0].ToString() + " " + DateTime.Now.ToLongTimeString());
+
+            if(Request.IsAjaxRequest())
+                return View("Lista", listaElementow);
+            else
+                return View();
+        }        
+
+    }
+}
