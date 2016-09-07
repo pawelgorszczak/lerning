df80e6760fbabe11e4a06d176f55a7e58bcd027c
pawelgorszczak <pawelgorszczakpl@gmail.com>
2016-09-07 17:14:54 +0200

    Regular Expressions

---
+++ ASP.NET MVC KURS/WyrazeniaRegularne/Controllers/DopasowaniaController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text.RegularExpressions;
+
+namespace WyrazeniaRegularne.Controllers
+{
+    public class DopasowaniaController : Controller
+    {
+        //
+        // GET: /Dopasowania/
+
+        static List<String> listaWyrazenDoSprawdzenia = new List<string>();
+        static List<String> wyrazeniaPasujaceDoWzorca = new List<string>();
+
+        public ActionResult Index(string wzorzec)
+        {
+            ViewData["ListaWyrazenDoSprawdzenia"] = new MultiSelectList(listaWyrazenDoSprawdzenia, wyrazeniaPasujaceDoWzorca);
+            ViewData["Wzorzec"] = wzorzec;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DodajElementDoSprawdzenia(FormCollection form)
+        {
+            if (form != null)
+                listaWyrazenDoSprawdzenia.Add(form["ElementDoSprawdzenia"].ToString());
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult WyczyscListe()
+        {
+            listaWyrazenDoSprawdzenia.Clear();
+            wyrazeniaPasujaceDoWzorca.Clear();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult SprawdzDopasowania(FormCollection form)
+        {
+            string wzorzec = form["Wzorzec"].ToString();
+
+            if (listaWyrazenDoSprawdzenia.Count > 0 && !string.IsNullOrEmpty(wzorzec))
+            {
+                wyrazeniaPasujaceDoWzorca.Clear();
+
+                foreach (var elementListy in listaWyrazenDoSprawdzenia)
+                {
+                    if (!Regex.IsMatch(elementListy, wzorzec))
+                        wyrazeniaPasujaceDoWzorca.Add(elementListy);
+                }
+            }
+            return RedirectToAction("Index", new { wzorzec });
+        }
+    }
+}
---
+++ ASP.NET MVC KURS/WyrazeniaRegularne/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WyrazeniaRegularne")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WyrazeniaRegularne")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51cef1a7-da3f-4749-8fab-e99f790c0309")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
